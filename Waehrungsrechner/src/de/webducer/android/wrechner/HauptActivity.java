b8478ce2dbe3528509a35294b2e3f07e33803f9d
Eugen Richter <info@webducer.de>
2011-09-17 19:56:23 +0200

    Init

---
+++ Waehrungsrechner/src/de/webducer/android/wrechner/HauptActivity.java
@@ -0,0 +1,13 @@
+package de.webducer.android.wrechner;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class HauptActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
